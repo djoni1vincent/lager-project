b2dc6a5ba83de2d9a0fc6c2f8adc1db2a7b6c9b7
djoni1vincent <djoni8vincent@gmail.com>
2025-11-24 19:31:20 +0100

    change bg for login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -10,9 +10,9 @@ function LoginPage() {
   return (
   <div className="text-center ">
     <h1 className="text-4xl mt-10 font-black">Logg inn</h1>
-    <input id="username" className="bg-gray-600 mt-25 text-center p-2 rounded-xl" type="text" placeholder="Username" /><br />
-    <input id="password" className="bg-gray-600 mt-5 text-center p-2 rounded-xl" type="text" placeholder="Password" /><br />
-    <button onClick={handleLogin} className="mt-10 bg-gray-600 px-5 py-3 rounded-xs hover:bg-gray-400 transition-color">Logg Inn</button>
+    <input id="username" className="bg-gray-200 mt-25 text-center p-2 rounded-xl" type="text" placeholder="Username" /><br />
+    <input id="password" className="bg-gray-200 mt-5 text-center p-2 rounded-xl" type="text" placeholder="Password" /><br />
+    <button onClick={handleLogin} className="mt-10 bg-gray-200 px-5 py-3 rounded-xs hover:bg-gray-400 transition-color">Logg Inn</button>
 
 </div>
   );
